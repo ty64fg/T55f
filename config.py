0576d273ca74a519de3de0b2a55ac011cb09ac76
ty64fg <ddosjoker6@gmail.com>
2024-12-18 21:00:01 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,19 @@
+# Don't use quotes( " and ' )
+#SCRIPT BY @KaliaYtOwner
+  
+#Enter Your Bot Token here get it from @botfarher
+BOT_TOKEN=("7997483159:AAHBxpVlK7qPOjYs_u0mMOaU0TRRJwbnk-I")
+
+  #Enter Your telegram username here without @
+OWNER_USERNAME=("@MG3_BEAST")
+
+  #Enter your admin id here Get it from @missRose_bot by typing /info
+ADMIN_IDS=("6373931635")
+
+
+
+
+
+
+
+  
